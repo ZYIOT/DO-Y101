fece4678fd98ebad32175ac0d070de7c997ee2f0
wsntuntun <wsntuntun@163.com>
2024-01-12 14:29:15 +0800

    second commit

--- bootloader/BSP/bsp_gpio.h
+++
@@ -1,6 +0,0 @@
-#include "gpio.h"
-#include "app.h"
-#include "hardware_wrapper.h"
-
-#define BSP_MBEDTLS_CHECK_STATUS()  (HARDWARE_GPIO_READ_PIN(MBEDTLS_CHECK_GPIO_Port, MBEDTLS_CHECK_Pin) == GPIO_PIN_RESET ? APP_OK : APP_ERROR)
-
--- bootloader/BSP/bsp_mcu.c
+++
@@ -1,9 +0,0 @@
-#include "bsp_mcu.h"
-
-
-int BSP_MCU_reset(void)
-{
-    __set_FAULTMASK(1);
-    HAL_NVIC_SystemReset();
-    return APP_OK;
-}
